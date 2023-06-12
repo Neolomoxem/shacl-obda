5d35cb570edbcfe4eb2d3ca588d3b1208effca83
Neolomoxem <maximilian.winkler@student.uni-luebeck.de>
2023-06-12 15:32:13 +0200

    great success

--- build/jars/ifis/ValidationFilter.java
+++
@@ -1,9 +0,0 @@
-package ifis;
-
-import java.util.stream.Stream;
-
-import org.apache.jena.sparql.engine.binding.Binding;
-
-public interface ValidationFilter {
-   public Stream<Binding> apply(Stream<Binding> s); 
-}
